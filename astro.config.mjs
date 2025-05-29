919e45dcad77284c536963a2e9d7be6012411281
mlabar7621 <mikhail.labar@gmail.com>
2025-05-29 23:37:05 +0200

    initial steps

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,19 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	i18n: {
+    	locales: ["en", "fr", "de", "ru", "be"],
+    	defaultLocale: "en",
+  	},
+	vite: {
+    resolve: {
+      alias: {
+        '@layouts': '/src/layouts',
+        '@components': '/src/components',
+		'@i18n': '/src/i18n',
+		'@consts': '/src/consts',
+		'@styles': '/src/styles',
+      }
+    }
+  }
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,44 @@
+export const languages = {
+  en: 'English',
+  de: 'German',
+};
+
+export const defaultLang = 'en';
+
+export const ui_text = {
+  en: {
+    nav:{
+        title: "Personal Site",
+        home: "Home",
+        about: "About"
+    }
+  },
+  de: {
+    nav:{
+        title: "Personelle Seite",
+        home: "Heim",
+        about: "Über Mich"
+    }
+  },
+  fr: {
+    nav:{
+        title: "Site Personel",
+        home: "Home",
+        about: "de Moi"
+    }
+  },
+  ru: {
+    nav:{
+        title: "Personal Site",
+        home: "Home",
+        about: "About"
+    }
+  },
+  be: {
+    nav:{
+        title: "Personal Site",
+        home: "Home",
+        about: "About"
+    }
+  }
+} as const;
